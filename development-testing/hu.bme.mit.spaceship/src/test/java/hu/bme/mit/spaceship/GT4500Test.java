cc3ea96b870df87639c99e44a55688ea62a04760
Koverm <koverm0414@gmail.com>
2017-10-29 21:38:04 +0100

    New tests

--- development-testing/hu.bme.mit.spaceship/src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ development-testing/hu.bme.mit.spaceship/src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -22,10 +22,111 @@ public class GT4500Test {
 	 * TODO: not a unit test, does not test the module in isolation!
 	 */
 	public void fireTorpedos_SinglePrimary_Success(){
-		boolean result = ship.fireTorpedos(FiringMode.SINGLE);
 		TorpedoStore pts = mock(TorpedoStore.class);
 		TorpedoStore sts = mock(TorpedoStore.class);
+		when(pts.isEmpty()).thenReturn(false);
+		when(sts.isEmpty()).thenReturn(true);
+		when(pts.fire(1)).thenReturn(true);
+		when(sts.fire(1)).thenReturn(true);
+		ship.setTorpedoStore(pts,sts);
+		boolean result = ship.fireTorpedos(FiringMode.SINGLE);
+
 		assertEquals(true, result);
+	}
+
+	@Test
+
+	public void fireTorpedos_SinglePrimary_Success_thenSecondaryFail(){
+		TorpedoStore pts = mock(TorpedoStore.class);
+		TorpedoStore sts = mock(TorpedoStore.class);
+		when(pts.isEmpty()).thenReturn(false).thenReturn(true);
+		when(sts.isEmpty()).thenReturn(false);
+		when(pts.fire(1)).thenReturn(true);
+		when(sts.fire(1)).thenReturn(true);
+		ship.setTorpedoStore(pts,sts);
+		ship.fireTorpedos(FiringMode.SINGLE);
+
+		//assertEquals(true, result);
+		boolean result  = ship.fireTorpedos(FiringMode.SINGLE);
+		assertEquals(false, result);
+	}
+	@Test
+
+	public void fireTorpedos_SinglePrimary_BothEmpty(){
+		TorpedoStore pts = mock(TorpedoStore.class);
+		TorpedoStore sts = mock(TorpedoStore.class);
+
+		when(pts.isEmpty()).thenReturn(true);
+		when(sts.isEmpty()).thenReturn(true);
+
+		when(pts.fire(1)).thenReturn(true);
+		when(sts.fire(1)).thenReturn(true);
+
+		ship.setTorpedoStore(pts,sts);
+
+		boolean result  = ship.fireTorpedos(FiringMode.SINGLE);
+		assertEquals(false, result);
+	}
+
+	@Test
+	public void	fireTorpedos_ALL_Succes(){
+
+		TorpedoStore pts = mock(TorpedoStore.class);
+		TorpedoStore sts = mock(TorpedoStore.class);
+		when(pts.isEmpty()).thenReturn(false);
+		when(sts.isEmpty()).thenReturn(false);
+		when(pts.fire(1)).thenReturn(true);
+		when(sts.fire(1)).thenReturn(true);
+		ship.setTorpedoStore(pts,sts);
+
+		boolean result = ship.fireTorpedos(FiringMode.ALL);
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void	fireTorpedos_ALL_SecondaryIsEmpty(){
+
+		TorpedoStore pts = mock(TorpedoStore.class);
+		TorpedoStore sts = mock(TorpedoStore.class);
+		when(pts.isEmpty()).thenReturn(false);
+		when(sts.isEmpty()).thenReturn(true);
+		when(pts.fire(1)).thenReturn(true);
+		when(sts.fire(1)).thenReturn(true);
+		ship.setTorpedoStore(pts,sts);
+
+		boolean result = ship.fireTorpedos(FiringMode.ALL);
+		assertEquals(true, result);
+	}
+
+
+	@Test
+	public void	fireTorpedos_ALL_Secondary_Fail(){
+
+		TorpedoStore pts = mock(TorpedoStore.class);
+		TorpedoStore sts = mock(TorpedoStore.class);
+		when(pts.isEmpty()).thenReturn(false);
+		when(sts.isEmpty()).thenReturn(false);
+		when(pts.fire(1)).thenReturn(true);
+		when(sts.fire(1)).thenReturn(false);
+		ship.setTorpedoStore(pts,sts);
+
+		boolean result = ship.fireTorpedos(FiringMode.ALL);
+		assertEquals(true, result);
+	}
+
+	@Test
+	public void	fireTorpedos_ALL_BothEmpty(){
+
+		TorpedoStore pts = mock(TorpedoStore.class);
+		TorpedoStore sts = mock(TorpedoStore.class);
+		when(pts.isEmpty()).thenReturn(true);
+		when(sts.isEmpty()).thenReturn(true);
+		when(pts.fire(1)).thenReturn(false);
+		when(sts.fire(1)).thenReturn(false);
+		ship.setTorpedoStore(pts,sts);
+
+		boolean result = ship.fireTorpedos(FiringMode.ALL);
+		assertEquals(false, result);
 	}
 	public class TestShip extends GT4500{
 		public void setTorpedoStore(TorpedoStore primaryTorpedoStore,TorpedoStore secondaryTorpedoStore){
